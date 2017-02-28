edd555587e72969801d2b29a110798ee903f2d8f
Kaczmarczyk Aleksander <194972@edu.p.lodz.pl>
2017-02-28 09:39:12 -0800

    Dodanie petli wstawiajacej nowe elementy do listy tails

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,10 @@ import java.util.List;
 public class TailsGenerator {
 
 	public List<String> tails(String value) {
-		List<String> tails = new ArrayList<>(value.length());		
+		List<String> tails = new ArrayList<>(value.length()+1);
+		for (int tailLength = value.length(); tailLength>=0; tailLength--) {
+			tails.add(value.substring(0, tailLength));
+		}
 		return tails;
 	}
 

fe9f53afb096d494173dcd94e7a0091062e2b0db
180273 <180273@CARBON.CTI.P.LODZ.PL>
2017-02-28 18:43:42 +0100

    One test passed

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -3,11 +3,11 @@ package edu.iis.mto.lab_0;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TailsGenerator {
-
-	public List<String> tails(String value) {
-		List<String> tails = new ArrayList<>(value.length());		
+public List<String> tails(String value) {
+		List<String> tails = new ArrayList<>(value.length());
+                for(int i=0; i<value.length(); i++){
+                    tails.add(value.substring(i, value.length()));
+                }
 		return tails;
 	}
-
 }

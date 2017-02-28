da4cef32668b2a322eae0fda6b5bd113f6fcc5f0
borzecki.andrzej <194918@edu.p.lodz.pl>
2017-02-28 17:53:19 +0100

    tailsShouldReturnFullStringAsFirstElement passes

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,8 @@ import java.util.List;
 public class TailsGenerator {
 
 	public List<String> tails(String value) {
-		List<String> tails = new ArrayList<>(value.length());		
+		List<String> tails = new ArrayList<>(value.length());
+		tails.add(value);
 		return tails;
 	}
 
